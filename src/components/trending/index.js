6d2dba49dc9f836ddcb9eb9d1915db8104fbe9f4
Ryan Harmuth <ryanharmuth@gmail.com>
2016-03-01 22:55:20 +0200

    V2 of tinder swipe

--- src/components/trending/index.js
+++ src/components/trending/index.js
@@ -1,24 +1,189 @@
 'use strict';
-import React, {
-  Component,
-  StyleSheet,
-  Text,
-} from 'react-native';
+
+import React, { AppRegistry, StyleSheet, Text, View, Animated, Component, PanResponder, Image} from 'react-native';
+import clamp from 'clamp';
+
+const Businesses = [
+  {name: 'Anglo American', image: 'http://www.entrepreneurmag.co.za/wp-content/uploads/2013/05/Anglo-American-Logo.gif'},
+  {name: 'Apple', image: 'http://images.apple.com/home/images/og.jpg?201602041611'},
+  {name: 'De Beers', image: 'http://www.3smedia.co.za/miningnews/wp-content/uploads/sites/3/2013/03/de-beers.jpg'},
+  {name: 'Microsoft', image: 'http://wacharters.org/wp-content/uploads/2015/01/new-microsoft-logo-SIZED-SQUARE-300x297.jpg'},
+  {name: 'Amazon', image: 'http://fullhdpictures.com/wp-content/uploads/2016/02/Different-Amazon-Logo.png'}
+]
+
+var SWIPE_THRESHOLD = 120;
 
 class Trending extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-
-      return (
-        <Text style={styles.title}>Trending</Text> 
-      );
+    this.state = {
+      pan: new Animated.ValueXY(),
+      enter: new Animated.Value(0.5),
+      business: Businesses[0],
     }
+  }
+
+  _goToNextPerson() {
+    let currentPersonIdx = Businesses.indexOf(this.state.business);
+    let newIdx = currentPersonIdx + 1;
+
+    this.setState({
+      business: Businesses[newIdx > Businesses.length - 1 ? 0 : newIdx]
+    });
+  }
+
+  componentDidMount() {
+    this._animateEntrance();
+  }
+
+  _animateEntrance() {
+    Animated.spring(
+      this.state.enter,
+      { toValue: 1, friction: 8 }
+    ).start();
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+        this.state.pan.setValue({x: 0, y: 0});
+      },
+
+      onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
+      ]),
+
+      onPanResponderRelease: (e, {vx, vy}) => {
+        this.state.pan.flattenOffset();
+        var velocity;
+
+        if (vx >= 0) {
+          velocity = clamp(vx, 3, 5);
+        } else if (vx < 0) {
+          velocity = clamp(vx * -1, 3, 5) * -1;
+        }
+
+        if (Math.abs(this.state.pan.x._value) > SWIPE_THRESHOLD) {
+          Animated.decay(this.state.pan, {
+            velocity: {x: velocity, y: vy},
+            deceleration: 0.98
+          }).start(this._resetState.bind(this))
+        } else {
+          Animated.spring(this.state.pan, {
+            toValue: {x: 0, y: 0},
+            friction: 4
+          }).start()
+        }
+      }
+    })
+  }
+
+  _resetState() {
+    this.state.pan.setValue({x: 0, y: 0});
+    this.state.enter.setValue(0);
+    this._goToNextPerson();
+    this._animateEntrance();
+  }
+
+  render() {
+    let { pan, enter, } = this.state;
+
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    let rotate = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: ["-30deg", "0deg", "30deg"]});
+    let opacity = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: [0.5, 1, 0.5]})
+    let scale = enter;
+
+    let animatedCardStyles = {transform: [{translateX}, {translateY}, {rotate}, {scale}], opacity};
+
+    let yupOpacity = pan.x.interpolate({inputRange: [0, 150], outputRange: [0, 1]});
+    let yupScale = pan.x.interpolate({inputRange: [0, 150], outputRange: [0.5, 1], extrapolate: 'clamp'});
+    let animatedYupStyles = {transform: [{scale: yupScale}], opacity: yupOpacity}
+
+    let nopeOpacity = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0]});
+    let nopeScale = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0.5], extrapolate: 'clamp'});
+    let animatedNopeStyles = {transform: [{scale: nopeScale}], opacity: nopeOpacity}
+
+    console.log(this.state)
+
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.card, animatedCardStyles]} {...this._panResponder.panHandlers}>
+          <Image source={{uri: this.state.business.image}} style={styles.cardImage} />
+            <View style={styles.cardTextContainer}>
+              <Text style={styles.textLeft}>{this.state.business.name}</Text>
+              <Text style={styles.textRight}>55$</Text>
+            </View>
+        </Animated.View>
+
+        <Animated.View style={[styles.nope, animatedNopeStyles]}>
+          <Text style={styles.nopeText}>Nope!</Text>
+        </Animated.View>
+
+        <Animated.View style={[styles.yup, animatedYupStyles]}>
+          <Text style={styles.yupText}>Yup!</Text>
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-    title: {
-      fontSize: 20,
-    },
-  });
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  card: {
+    width: 200,
+    height: 200,
+  },
+  yup: {
+    borderColor: 'green',
+    borderWidth: 2,
+    position: 'absolute',
+    padding: 20,
+    bottom: 20,
+    borderRadius: 5,
+    right: 20,
+  },
+  yupText: {
+    fontSize: 16,
+    color: 'green',
+  },
+  nope: {
+    borderColor: 'red',
+    borderWidth: 2,
+    position: 'absolute',
+    bottom: 20,
+    padding: 20,
+    borderRadius: 5,
+    left: 20,
+  },
+  nopeText: {
+    fontSize: 16,
+    color: 'red',
+  },
+  cardImage: {
+    height: 260,
+  },
+  textLeft: {
+    position: 'absolute',
+    left:0,
+    top:0
+  },
+  textRight: {
+    position: 'absolute',
+    right: 0,
+    top: 0
+  }
+});
+
 
 module.exports = Trending
