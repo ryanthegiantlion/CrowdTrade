964f578da343729810eb75d74c7cdb3e5288f3d0
Ryan Harmuth <ryanharmuth@gmail.com>
2016-04-13 18:12:08 +0200

    Added crowd chat questions list

--- src/components/crowdchat/index.js
+++ src/components/crowdchat/index.js
@@ -4,10 +4,13 @@ import React, {
   StyleSheet,
   Text,
   View,
+  ListView,
 } from 'react-native';
 import Search from '../shared/search/index'
 import CrowdChatTabBar from './tabBar'
+import { connect } from 'react-redux'
 var ScrollableTabView = require('react-native-scrollable-tab-view');
+
 
 class AskContainer extends Component {
   render() {
@@ -19,11 +22,37 @@ class AskContainer extends Component {
   }
 }
 
+class QuestionItem extends Component {
+  render() {
+    return (
+      <View style={styles.questionItemContainer}>
+        <Text style={styles.number}>{this.props.id + "."}</Text>
+        <View style={styles.titleAndDescriptionContainer}>
+          <Text style={styles.title}>{this.props.title}</Text>
+          <Text style={styles.description}>{this.props.description}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
 class QuestionsContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(this.props.chats),
+    };
+  }
+
   render() {
     return (
       <View style={styles.questionContainer}>
-        <Text style={styles.tempHeading}>{this.props.title}</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <QuestionItem {...rowData} />}
+          renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}/>
       </View>
     )
   }
@@ -36,9 +65,9 @@ class CrowdChat extends Component {
       <View style={styles.bodyContainer}>
         <Search title='Crowd chat' />
         <ScrollableTabView contentProps={{bounces: false}} initialPage={0} renderTabBar={() => <CrowdChatTabBar />}>
-          <QuestionsContainer key="HOT" title='HOT' tabLabel='HOT'/>
-          <QuestionsContainer key="NEW" title='NEW' tabLabel='NEW'/>
-          <QuestionsContainer key="TOP" title='TOP' tabLabel='TOP'/>
+          <QuestionsContainer chats={this.props.chats} key="HOT" title='HOT' tabLabel='HOT'/>
+          <QuestionsContainer chats={this.props.chats} key="NEW" title='NEW' tabLabel='NEW'/>
+          <QuestionsContainer chats={this.props.chats} key="TOP" title='TOP' tabLabel='TOP'/>
           <AskContainer key="ASK" title='ASK' tabLabel='ASK'/>
         </ScrollableTabView>
       </View>
@@ -56,9 +85,42 @@ const styles = StyleSheet.create({
       flex: 1,
     },
 
-    tempHeading: {
+    questionItemContainer: {
+      flexDirection: 'row',
+      padding: 20,
 
+    },
+
+    number: {
+      marginRight: 10,
+      width: 20,
+      fontWeight: 'bold',
+    },
+
+    titleAndDescriptionContainer: {
+      flex: 1,  
+    },
+
+    title: {
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+
+    description: {
+      color: '#999',
+      fontStyle: 'italic',
+    },
+
+    separator: {
+      height: 1,
+      backgroundColor: '#888',
     },
   });
 
-module.exports = CrowdChat
+function mapStateToProps(state) {
+  return {
+    chats: state.crowdChat.data,
+  }
+}
+
+module.exports = connect(mapStateToProps)(CrowdChat);
